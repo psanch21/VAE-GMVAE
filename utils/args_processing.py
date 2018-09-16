33f27633b21d3b2c9b0bf6cddb382464885f22a7
Pablo <psanch2103@gmail.com>
2018-09-16 19:17:20 +0200

    dir arguments updated

--- utils/args_processing.py
+++ utils/args_processing.py
@@ -2,7 +2,7 @@ import json
 from bunch import Bunch
 import os
 import argparse
-import utils.constants as const 
+import utils.constants as const
 
 
 def parser_basic():
@@ -10,17 +10,17 @@ def parser_basic():
     parser = argparse.ArgumentParser(description=desc)
 
     parser.add_argument('--epochs', type=int, default=20,  help='Number of epochs for training')
-    parser.add_argument('--batch_size', type=int, default=64, help='Number of inputs used for each iteration')  
-    parser.add_argument('--l_rate', type=float, default=1e-3, help='Parameter of the optimization function') 
+    parser.add_argument('--batch_size', type=int, default=64, help='Number of inputs used for each iteration')
+    parser.add_argument('--l_rate', type=float, default=1e-3, help='Parameter of the optimization function')
     parser.add_argument('--drop_prob', type=float, default=0, help='Dropout regularizer parameter')
-    
+
     # hidden units in the NNs
     parser.add_argument('--hidden_dim', type=int, default=50, help='Number of neurons of each dense layer')
     parser.add_argument('--num_layers', type=int, default=2, help='Number of dense layers in each network')
 
-    parser.add_argument('--checkpoint_dir', type=str, default='saved_models', help='Directory name to save the checkpoints')
+    parser.add_argument('--checkpoint_dir', type=str, default='checkpoint', help='Directory name to save the checkpoints')
     parser.add_argument('--result_dir', type=str, default='results', help='Directory name to save the generated images')
-    parser.add_argument('--logdir', type=str, default='logdir', help='Directory name to save training logs')
+    parser.add_argument('--log_dir', type=str, default='summary', help='Directory name to save training logs')
     parser.add_argument('--dataset_name', type=str, default='MNIST or FREY')
 
     parser.add_argument('--train', type=int, default=1, help='Flag to set train')
@@ -33,7 +33,7 @@ def parser_basic():
 
     parser.add_argument('--model_type', type=int, default=0, help='Fixes the model and architecture')
     parser.add_argument('--extra', type=str, default='', help='Extra name to identify the model')
-    
+
     parser = parser_VAE(parser)
     parser = parser_GMVAE(parser)
     return parser
@@ -43,7 +43,7 @@ def parser_VAE(parser):
     parser.add_argument('--z_dim', type=int, default=3, help='Dimension of the latent variable z')
     parser.add_argument('--beta', type=float, default=1, help='Beta parameter in the KL')
     return parser
-    
+
 def parser_GMVAE(parser):
     parser.add_argument('--w_dim', type=int, default=3, help='Dimension of the latent variable w. Only for GMVAE')
     parser.add_argument('--K_clusters', type=int, default=10, help='Number of modes of the latent variable z. Only for GMVAE')
@@ -101,26 +101,26 @@ def get_config_from_json(json_file):
 
 def process_args(args,model):
     config = Bunch(args)
-    
+
     if(config.model_type==const.VAE or config.model_type==const.VAECNN):
         config.model_name = get_model_name_VAE(model, config)
     if(config.model_type==const.GMVAE or config.model_type==const.GMVAECNN):
         config.model_name = get_model_name_GMVAE(model, config)
-    
+
     if(config.extra is not ''):
         config.model_name += '_' + config.extra
-        
-    config.summary_dir = os.path.join("experiments/summary/", config.model_name)
-    config.checkpoint_dir = os.path.join("experiments/checkpoint/", config.model_name)
-    config.results_dir = os.path.join("experiments/results/", config.model_name)
-    
+
+    config.summary_dir = os.path.join("experiments/"+config.log_dir+"/", config.model_name)
+    config.checkpoint_dir = os.path.join("experiments/"+config.checkpoint_dir+"/", config.model_name)
+    config.results_dir = os.path.join("experiments/"+config.result_dir+"/", config.model_name)
+
     flags = Bunch()
     flags.train = args['train']
     flags.restore = args['restore']
     flags.results = args['results']
     flags.plot = args['plot']
     flags.early_stopping =  args['early_stopping']
-    
+
     return config, flags
 
 def get_model_name_VAE(model, config):
@@ -142,7 +142,7 @@ def get_model_name_GMVAE(model, config):
                  str(config.hidden_dim)  + '_'+\
                  str(config.num_layers)
     return model_name
-                        
+
 def get_config_and_flags(args):
     aux_name = ''
     if(args.model_type==const.VAE):
@@ -155,6 +155,5 @@ def get_config_and_flags(args):
         aux_name+='GMVC'
 
     # aux_name+= '_'+str(int(args.beta))
-    
-    return process_args(vars(args), aux_name)  
 
+    return process_args(vars(args), aux_name)
